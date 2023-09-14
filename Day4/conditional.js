4ead827eae90071dad360ca5f6344f9641de5fb9
satheshj <satheshjr6@gmail.com>
2023-09-14 18:05:43 +0530

    conditional statements

---
+++ Day4/conditional.js
@@ -0,0 +1,12 @@
+// Simple-if
+a=20;
+if(a<18){
+    console.log("Innum valaranum thambi");
+}
+//if-else
+if(prompt("Enter the number")%2==0){
+    alert("Given number is even")
+}
+else{
+    alert("Given number is odd")
+}
