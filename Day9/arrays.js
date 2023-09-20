12b5a167a0871153ad4092c8c9ebdd7ec115b28c
satheshj <satheshjr6@gmail.com>
2023-09-20 18:37:40 +0530

    Arrays

---
+++ Day9/arrays.js
@@ -0,0 +1,6 @@
+//Creating with list brackets or square brackets
+var fruits = ["apple","strawberry","watermelon"];
+console.log(fruits);
+//by constructor
+var color = new Array("Red","Maroon","white");
+console.log(color);
