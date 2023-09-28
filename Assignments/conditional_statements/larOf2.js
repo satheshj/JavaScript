733c024a52837eab3581c42fb12e4b5c864014b3
satheshj <satheshjr6@gmail.com>
2023-09-28 20:40:52 +0530

    JS assignments

---
+++ Assignments/conditional_statements/larOf2.js
@@ -0,0 +1,13 @@
+var a = prompt("Enter the first number");
+var b = prompt("Enter the second number");
+if(a>b)
+{
+    alert(a+" is bigger");
+}
+else if(a==b)
+{
+    alert("Both numbers are equal");
+}
+else{
+    alert(b+ " is bigger")
+}
---
+++ Assignments/conditional_statements/odd_even_n.js
@@ -0,0 +1,10 @@
+var a = 0;
+while(a <= 15){
+    if(a %2 == 0){
+        console.log(a+" is an even number")
+    }
+    else{
+        console.log(a+" is an odd number");
+    }
+    a++;
+}
