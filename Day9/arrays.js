21e8f2c105fe760a09688ec0331df17dea249e10
satheshj <satheshjr6@gmail.com>
2023-10-02 14:35:27 +0530

    Arrays

--- Day9/arrays.js
+++ Day9/arrays.js
@@ -4,3 +4,8 @@ console.log(fruits);
 //by constructor
 var color = new Array("Red","Maroon","white");
 console.log(color);
+arr1 = [1,2,3];
+arr2 = ["red","yellow"];
+arr3 = [arr1,arr2] // (2) [Array(3), Array(2)]
+arr4 = [...arr1,...arr2]; // [1, 2, 3, 'red', 'yellow']
+
