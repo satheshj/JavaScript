73d3fc9167093e93ed6cc1f4dc16d097070c4678
satheshj <satheshjr6@gmail.com>
2023-10-02 14:39:30 +0530

    Objects

--- Day11/creationOfObject.js
+++ Day11/creationOfObject.js
@@ -8,3 +8,18 @@ var Student = {
 console.log(Student.age); // 22
 //accessing non-valid identifier by using[]
 console.log(Student["location of the student"]); //Coimbatore
+
+let person = {
+    "Name" : "sathesh",
+    "age" : 21
+}
+
+let location1 = {
+    "city" : "coimbatore",
+    "pin" : 641046
+}
+let person_details = {
+    ...person,
+    ...location
+}
+console.log(person_details); // {Name: 'sathesh', age: 21, city: 'coimbatore', pin: 641046}
