2ff4473b7fb07fc2b738d570004a0c8f84d58396
satheshj <satheshjr6@gmail.com>
2023-10-02 14:37:54 +0530

    Loops and function

--- Day5/function.js
+++ Day5/function.js
@@ -1,4 +1,5 @@
 //Function declaration syntax
+//Traditional function - can be called before definition
 //No argument and no return value function
 function disp(){
     document.write("Hello World");
@@ -12,3 +13,31 @@ n = parseInt(prompt("Enter thr number"));
 function squareof(n){
     return n**2; // return the squareof
 }
+
+//Anonymous or expression - cannot be called before definition
+let disp = function(){
+    console.log("Hello World")
+}
+disp(); //Hello World
+
+//Arrow Function - cannot be called before definition
+let disp = (a) =>{
+    console.log("This is arrow function" + a);
+}
+disp("Hello World"); // This is arrow functionHello World
+
+let hello = () =>{
+    console.log("Hello World");
+}
+
+//                              callback function -- passing function as an argument for other function
+
+console.log(hello()); //Hello World
+                    // undefined
+
+a = [1,2,3,4,5,6];
+let res = a.filter((i) =>{
+    return i<=3;
+})
+console.log(res); //  [1, 2, 3]
+
